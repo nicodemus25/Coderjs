49a5b7392e7984a08962aa00cfa92556818dd0c8
nicopc <nicofein31@gmail.com>
2023-07-12 16:03:07 -0300

    primer commit js

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// calculadora de precios
+
+const suma = (precio1, precio2) => precio1 + precio2;
+let precioProducto1 = 500;
+let precioProducto2 = 500;
+let precioFinal = suma(precioProducto1,precioProducto2)
+
+console.log(precioFinal);
+
+
+
+
+
+
+    
+   
+
+ 
+ 
