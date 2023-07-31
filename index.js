38656b36364984d10f941cd4048e926b93e7de68
nicopc <nicofein31@gmail.com>
2023-07-31 20:28:49 -0300

    commiteo se que esta mal

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 
 // calculadora de precios
 
-const suma = (precio1, precio2) => precio1 + precio2;
-let precioProducto1 = 500;
-let precioProducto2 = 500;
-let precioFinal = suma(precioProducto1,precioProducto2)
+let calculadora = 0;
 
-console.log(precioFinal);
+let precios = [];
 
+let precioEstimado = parseInt(prompt("Indique el precio"));
 
+let precioTotal = 0
+console.log(precios);
 
+precios.push(precioEstimado);
 
+console.log(precios);
 
+let preciofinal = parseInt(prompt("Indique el precio"));
 
-    
-   
+ precios.push(preciofinal);
 
- 
- 
+ console.log(precios);
+
+ const total = precios.reduce((acumulador, elemento) => acumulador + elemento, 0)
+
+ alert("el precio final es " + total);
